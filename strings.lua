edb43666856499f58843efa99d1adf471b7a3678
lolluslollus <lolluslollus@outlook.com>
2020-10-24 01:06:45 +0200

    docu

--- strings.lua
+++ strings.lua
@@ -3,7 +3,15 @@ return {
 	en = {
 		NAME = "Town tuning",
 		DESC = [[Modifies town building requirements and capacities.
-		This mod adds a \"tuning\" tab to the town menus, where you can alter some parameters in new and old games"]],
+		This mod adds a \"tuning\" tab to the town menus, where you can alter some parameters in new and old games.
+		If your game has grown and ground to a halt, you can reduce the industrial and commercial requirements across all towns.
+		If you want more, you can reduce people requirements in every building.
+		You can also do the opposite and increase the parameters, if you have a NASA computer or a very small game.
+
+		As a bonus, you get new switches to avoid new skyscrapers, build new roads with non-square corners or curb the geometry option.
+		As another bonus, you can add more requirements to residential, commercial and industrial areas, town by town.
+
+		NOTE: Be patient when you alter a parameter that affects all towns, it might take a while before the job is done.]],
 		CAPACITY_FACTOR = "Industrial and commercial capacity factor, affects all towns",
 		CONSUMPTION_FACTOR = "Industrial and commercial consumption factor, affects all towns",
 		FASTER_LOW_GEOMETRY = "Faster low geometry setting",
